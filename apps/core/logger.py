d12534b5bd0eda67839598fbc7d186903f5d56b1
Ankur Lamba <ankur@MBP23.local>
2023-10-17 20:28:51 -0500

    fix logs

--- apps/core/logger.py
+++ apps/core/logger.py
@@ -28,9 +28,9 @@ class Logger:
         self.logger = logging.getLogger(str(log_module)+'_' + str(run_id))
         self.logger.setLevel(logging.DEBUG)
         if log_file_name=='training':
-            file_handler = logging.FileHandler('/logs/training_logs/train_log_' + str(run_id) + '.log')
+            file_handler = logging.FileHandler('logs/training_logs/train_log_' + str(run_id) + '.log')
         else:
-            file_handler = logging.FileHandler('/logs/prediction_logs/predict_log_' + str(run_id) + '.log')
+            file_handler = logging.FileHandler('logs/prediction_logs/predict_log_' + str(run_id) + '.log')
 
         formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s')
         file_handler.setFormatter(formatter)
